6afc39320b58e749db571f7771570b8e449a2836
NastifyMe <suprunar2010@gmail.com>
2026-01-30 11:58:16 +0100

    млкь

--- pixabay-api.js
+++ pixabay-api.js
@@ -1,10 +1,22 @@
-export const fetchApi = async (value, per_page) => {
-    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${encodeURIComponent(
-      value
-    )}&page=1&per_page=${per_page}&key=53972263-c9e9823441f8f52f69cf141bb`;
-  
+const API_KEY = "53972263-c9e9823441f8f52f69cf141bb";
+const BASE_URL = "https://pixabay.com/api/";
+
+export async function fetchApi(query, perPage) {
+  try {
+    const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
+      query
+    )}&image_type=photo&orientation=horizontal&page=1&per_page=${perPage}`;
+
     const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("API error");
+    }
+
     const data = await response.json();
     return data.hits;
-  };
-  
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
