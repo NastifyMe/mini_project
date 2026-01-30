a9e30626bb5f1f41c21dd7437aa3fe4b721ac8b2
NastifyMe <suprunar2010@gmail.com>
2026-01-30 11:51:16 +0100

    kdw

--- pixabay-api.js
+++ pixabay-api.js
@@ -1,6 +1,10 @@
-export const fetchApi = async(value, per_page) => {
-    return await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=1&per_page=${per_page}&key=53972263-c9e9823441f8f52f69cf141bb`)
-        .then(res => res.json())
-        .then(res => res.hits)
-};
-
+export const fetchApi = async (value, per_page) => {
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${encodeURIComponent(
+      value
+    )}&page=1&per_page=${per_page}&key=53972263-c9e9823441f8f52f69cf141bb`;
+  
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.hits;
+  };
+  
